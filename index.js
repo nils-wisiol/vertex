a662c9ee4fa619aaaf4d573c229c96ab4bced917
Nils Wisiol <info@nils-wisiol.de>
2014-09-04 16:07:46 -0400

    fix(addNode): fixed pwd() function
    It has been overridden previously.

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ function addNode(config) {
 	nginx.addRevProxy(config.appid, config.hostname);
 	
 	//// 400 git clone / deployment
-	var pwd = exec('pwd');
+	var dir = pwd();
 	cd(config.documentroot);
 	exec('git clone --depth=1 \'' + config.git + '\' .'); // TODO correct branch TODO authentication TODO compiliation and deployment
-	cd(pwd);
+	cd(dir);
 	
 	//// 500 Create upstart job
 	upstart.addJob(config.upstart, config.documentroot, config.unixuser, config.unixid, config.appid, config.hostname);
